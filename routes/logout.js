b7438c9dd2983e6438dd6821afa06a3c1715418e
Mazvydas <mjaniskevicius@gmail.com>
2023-09-07 11:44:02 +0300

    Removed unnecessary files + bug fixes

--- routes/logout.js
+++ routes/logout.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const ensureAuthenticated = require('../config/ensureAuthenticated')
 
 router.get('/', ensureAuthenticated, (req, res) => {
-    req.logout()
-    res.redirect('/')
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
